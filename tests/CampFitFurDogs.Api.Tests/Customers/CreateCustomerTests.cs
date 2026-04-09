5c15655349a79aa5b2dd09e451972e9e30dc6b7a
Frank J Hughes <frank.j.hughes@outlook.com>
2026-04-09 13:34:20 -0700

    feature: create customer - passing test + api + application sans infrastructucture

---
+++ tests/CampFitFurDogs.Api.Tests/Customers/CreateCustomerTests.cs
@@ -0,0 +1,41 @@
+using System.Net;
+using System.Net.Http.Json;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+using CampFitFurDogs.Api;
+
+namespace CampFitFurDogs.Api.Tests.Customers;
+
+public class CreateCustomerTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+
+    public CreateCustomerTests(WebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task CreateCustomer_ShouldReturn201AndCustomerId()
+    {
+        var request = new
+        {
+            FirstName = "Frank",
+            LastName = "Hughes",
+            Email = "frank@example.com",
+            Phone = "555-1234",
+            Password = "SuperSecure123!"
+        };
+
+        var response = await _client.PostAsJsonAsync("/api/customers", request);
+
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+
+        var body = await response.Content.ReadFromJsonAsync<CreateCustomerResponse>();
+        body.Should().NotBeNull();
+        body!.CustomerId.Should().NotBe(Guid.Empty);
+    }
+
+    public sealed record CreateCustomerResponse(Guid CustomerId);
+}
