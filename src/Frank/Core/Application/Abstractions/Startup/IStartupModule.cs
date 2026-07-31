baa78de289efbde440b3fcf5c045a3d8f3816807
Frank J Hughes <frank.j.hughes@outlook.com>
2026-07-31 12:18:50 -0700

    refactor: reorganize frank identity service registration

--- src/Frank/Core/Application/Abstractions/Startup/IStartupModule.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Builder;
-
-namespace Frank.Core.Application.Abstractions.Startup;
-
-public interface IStartupModule
-{
-    void Add(WebApplicationBuilder builder);
-    void Use(WebApplication app);
-}
